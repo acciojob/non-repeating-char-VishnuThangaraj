38c6ede89f6b994a38b5ac10b21ac25d72b6389a
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-22 18:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const map = new Map();
+
+	for(let letter of str){
+		if(map.has(letter)){
+			map.set(letter, map.get(letter)+1);
+		}
+		else{
+			map.set(letter, 1);
+		}
+	}
+
+	for(let letter of str){
+		
+		if(map.get(letter)==1) return letter;
+	}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
